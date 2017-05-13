a12d7150102197123232d8dc603424ba3daa3efe
Alexander <fm107@ya.ru>
2017-05-13 16:26:29 +0300

    Update utorrent.sh

--- bin/utorrent.sh
+++ bin/utorrent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /app/utorrent-server/ && ./utserver -settingspath utserver.conf -logfile /app/heroku_output/wwwroot/uploads/log.txt -daemon
+cd /app/utorrent-server/ && ./utserver -configfile utserver.conf -logfile /app/heroku_output/wwwroot/uploads/log.txt -daemon
