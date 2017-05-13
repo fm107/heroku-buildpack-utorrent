4ab5f995b6cde0b7db23f04b091bcfa4802e0f3d
Alexander <fm107@ya.ru>
2017-05-13 15:59:08 +0300

    Update utorrent.sh

--- bin/utorrent.sh
+++ bin/utorrent.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-mkdir -p /app/utorrent-server/incoming/incomplete
-mkdir -p /app/utorrent-server/incoming/complete
-mkdir -p /app/utorrent-server/incomplete/temp
-mkdir -p /app/utorrent-server/incoming/torrents
-mkdir -p /app/utorrent-server/autoload
-
 cd /app/utorrent-server/ && ./utserver -settingspath utserver.conf -logfile /app/heroku_output/wwwroot/uploads/log.txt -daemon
